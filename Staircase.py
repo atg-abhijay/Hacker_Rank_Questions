f9f01aedd24171f112ec383c4a1cebbf107bcd69
atg-abhijay <atg.abhijay@gmail.com>
2017-11-28 23:35:00 -0500

    printing a right-aligned 'staircase' of size n

--- Staircase.py
+++ Staircase.py
@@ -2,11 +2,17 @@ def main():
     # enter a number 'n' as an argument
     # and the program will print a
     # 'staircase' of size n
+    # example - staircase of size 5
+    #     *
+    #    **
+    #   ***
+    #  ****
+    # ***** (no extra space before first *)
     n = int(raw_input().strip())
     arr = [" "] * n
     for i in range(n):
         # print(i)
-        arr[n-1-i] = "#"
+        arr[n-1-i] = "*"
         print(str(''.join(arr)))
 
 main()
