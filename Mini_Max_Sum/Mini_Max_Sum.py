d152de449417c48c44795f46301044f8b10e41a5
atg-abhijay <atg.abhijay@gmail.com>
2017-11-28 22:40:18 -0500

    made my own version of the question

--- Mini_Max_Sum/Mini_Max_Sum.py
+++ Mini_Max_Sum/Mini_Max_Sum.py
@@ -8,4 +8,18 @@ def main():
 
     print min(sums), max(sums)
 
-main()
+
+# let array be -    a0      a1      a2      a3      a4
+# let sums be -  (a3+ a4) (a4+a0) (a0+a1) (a1+a2) (a2+a3)
+def own_question():
+    arr = [1,2,3,4,5]
+    sums = [sum(arr)] * len(arr)
+    for x in range(len(arr)):
+        first_indice = (x+3)%len(arr)
+        second_indice = (x+4)%len(arr)
+        sums[x] = arr[first_indice] + arr[second_indice]
+
+    print(sums)
+
+# main()
+own_question()
